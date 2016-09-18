dfcd2199a518746c455f65b21c3af4a799258a38
Dom Farnham <dominic.farnham@hotmail.co.uk>
2016-09-18 21:50:51 +0100

    deleted test files 1 and 2. Added some comments to app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./mock/posts.json');
+
+var app = express();
+
+// Route for the root of the site
+app.get('/', function(req, res){
+	res.send("<h1>I am IN Love with Treehouse!</h1>");
+});
+
+// Route for the blog page
+app.get('/blog', function(req, res){
+	res.send(posts);	
+});
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!");
+});
